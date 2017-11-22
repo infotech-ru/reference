e0ebeaa31e6f35fd0e03cca09a095e11b675b0f2
Vladimir Vyachin <vladimir@vyachin.net>
2017-11-22 18:17:39 +0300

    sources

---
+++ tests/unit/models/SourceTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace infotech\reference\tests\unit\models;
+
+
+use infotech\reference\models\Source;
+use infotech\reference\models\SourceQuery;
+use PHPUnit\Framework\TestCase;
+
+class SourceTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $this->assertNotNull(new Source());
+    }
+
+    public function testTableName()
+    {
+        $this->assertEquals('sources', Source::tableName());
+    }
+
+    public function testFind()
+    {
+        $this->assertInstanceOf(SourceQuery::class, Source::find());
+    }
+
+    public function testGetParent()
+    {
+        $model = new Source();
+        $this->assertInstanceOf(SourceQuery::class, $model->getParent());
+    }
+
+    public function testAttributes()
+    {
+        $model = new Source();
+        $this->assertEquals(
+            [
+                'id',
+                'name',
+                'parent_id',
+            ],
+            $model->attributes()
+        );
+    }
+}
