0780e48c2404e10e8d69f5ee9aa904bcd5005158
Никита Медведев <n@raidkon.com>
2018-10-08 14:31:47 +0500

    #2170

---
+++ migrations/m181008_091703_tbl_brand_add_col_vin_man.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181008_091703_tbl_brand_add_col_vin_man
+ */
+class m181008_091703_tbl_brand_add_col_vin_man extends Migration
+{
+    const TBL = 'brands';
+    const COL = 'is_vin_manufacturer';
+    
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn(self::TBL,self::COL,$this->integer(1)->defaultValue(0)->comment('Ипользуется ли вин производителя'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn(self::TBL,self::COL);
+        return true;
+    }
+}
