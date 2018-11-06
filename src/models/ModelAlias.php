8b66b1ed12de7676ce10e8520ab4950fb4332292
Vladimir Vyachin <vladimir@vyachin.net>
2018-11-06 11:56:38 +0300

    #6219 Реализовать алиасы для моделей

---
+++ src/models/ModelAlias.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace infotech\reference\models;
+
+/**
+ * Class ModelAlias
+ * @package infotech\reference\models
+ * @property integer $id
+ * @property string $name
+ * @property integer $brand_id
+ * @property integer $model_id
+ * @property integer $generation_id
+ * @property integer $serie_id
+ * @property integer $status
+ * @property string $created_at
+ * @property string $updated_at
+ * @property-read Brand $brand
+ * @property-read Model $model
+ * @property-read Generation $generation
+ * @property-read Serie $serie
+ */
+class ModelAlias extends ActiveRecord
+{
+    const STATUS_ACTIVE = 0;
+    const STATUS_DELETED = 1;
+
+    public static function tableName(): string
+    {
+        return 'model_alias';
+    }
+
+    public static function getList($brandId): array
+    {
+        return static::find()->brand($brandId)->status(self::STATUS_ACTIVE)->select('name, id')->indexBy('id')->column(
+        );
+    }
+
+    public static function find()
+    {
+        return new ModelAliasQuery(static::class);
+    }
+
+    public function getBrand()
+    {
+        return $this->hasOne(Brand::class, ['id' => 'brand_id']);
+    }
+
+    public function getModel()
+    {
+        return $this->hasOne(Model::class, ['id' => 'model_id']);
+    }
+
+    public function getGeneration()
+    {
+        return $this->hasOne(Generation::class, ['id_car_generation' => 'generation_id']);
+    }
+
+    public function getSerie()
+    {
+        return $this->hasOne(Serie::class, ['id_car_serie' => 'serie_id']);
+    }
+}
---
+++ tests/fixtures/ModelAliasFixture.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\fixtures;
+
+use infotech\reference\models\ModelAlias;
+use yii\test\ActiveFixture;
+
+class ModelAliasFixture extends ActiveFixture
+{
+    public $modelClass = ModelAlias::class;
+    public $depends = [BrandFixture::class, ModelFixture::class, GenerationFixture::class, SerieFixture::class];
+    public $db = 'ref_db';
+}
