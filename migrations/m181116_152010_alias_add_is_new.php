184902b0d976e4c8a4631e27150f7f6ea0bf6cd8
Никита Медведев <n@raidkon.com>
2018-11-16 20:23:13 +0500

    autocrm-6298 РефБД, алиасы

---
+++ migrations/m181116_152010_alias_add_is_new.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181116_152010_alias_add_is_new
+ */
+class m181116_152010_alias_add_is_new extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('model_alias','is_new',$this->boolean()->defaultValue(false));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('model_alias','is_new');
+    }
+}
