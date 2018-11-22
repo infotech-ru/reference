fe92ce384d269f62cf37af4f23653088beaec10c
Vladimir Vyachin <vladimir@vyachin.net>
2018-11-22 15:31:18 +0300

    #6234 Новости реф

--- src/models/News.php
+++ src/models/News.php
@@ -2,6 +2,17 @@
 
 namespace infotech\reference\models;
 
+/**
+ * Class News
+ * @package infotech\reference\models
+ * @property integer $id
+ * @property string $date_public
+ * @property integer $tags_bitmap
+ * @property string $title
+ * @property string $content
+ * @property-read NewsBrand[] $newsBrands
+ * @property-read Brand[] $brands
+ */
 class News extends ActiveRecord
 {
     public static function tableName(): string
