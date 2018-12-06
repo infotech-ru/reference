3fe9183a92a1fef9bbf070eea0327ad36a4601e9
Vladimir Vyachin <vladimir@vyachin.net>
2018-12-06 12:07:14 +0300

    #6391 Реф база (#62)
    * #6391 Реф база
    
    * #6391 Реф база

---
+++ migrations/m181206_084257_create_table_skin_serie.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181206_084257_create_table_skin_serie
+ */
+class m181206_084257_create_table_skin_serie extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('skin_serie', [
+            'skin_id' => $this->integer()->notNull(),
+            'serie_id' => $this->integer()->notNull(),
+            'PRIMARY KEY(skin_id, serie_id)',
+        ], 'ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci');
+        $this->addForeignKey('fk_skin_serie_skin_id', 'skin_serie', 'skin_id', 'eqt_skin', 'id');
+        $this->addForeignKey('fk_skin_serie_serie_id', 'skin_serie', 'serie_id', 'car_serie', 'id_car_serie');
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('skin_serie');
+    }
+}
---
+++ src/models/SkinSerie.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace infotech\reference\models;
+
+/**
+ * Class SkinSerie
+ * @package infotech\reference\models
+ * @property integer $skin_id
+ * @property string $serie_id
+ * @property-read Skin $skin
+ * @property-read Serie $serie
+ */
+class SkinSerie extends ActiveRecord
+{
+    public static function tableName(): string
+    {
+        return 'skin_serie';
+    }
+
+    public static function find()
+    {
+        return new SkinSerieQuery(static::class);
+    }
+
+    public function getSkin()
+    {
+        return $this->hasOne(Skin::class, ['id' => 'skin_id']);
+    }
+
+    public function getSerie()
+    {
+        return $this->hasOne(Serie::class, ['id_car_serie' => 'serie_id']);
+    }
+}
