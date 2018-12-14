e2a944d31ab5217f050c8b50f1ecc63ec6bc01b2
Oleg Zaikin <zord@mail.ru>
2018-12-14 15:52:44 +0300

    Добавлено поле name_market (#68)
    * Добавлено поле name_market

---
+++ migrations/m181214_122605_add_column_brand_name_market.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181214_122605_add_column_brand_name_market
+ */
+class m181214_122605_add_column_brand_name_market extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('brands', 'name_market', $this->string()->notNull()->after('name_eng')->comment('Торговое наименование'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('brands', 'name_market');
+    }
+}
