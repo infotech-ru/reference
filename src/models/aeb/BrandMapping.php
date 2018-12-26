69551317c860ade8f42d736e0ae04fb419c6f7d8
Никита Медведев <n@raidkon.com>
2018-12-27 04:47:03 +0500

    autocrm-6510 Импорт АЕБ (AEB) - маппинг брендов

---
+++ src/models/aeb/BrandMapping.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Raidkon
+ * Date: 26.12.2018
+ * Time: 17:06
+ */
+
+namespace infotech\reference\models\aeb;
+
+use Yii;
+use infotech\reference\models\ActiveRecord;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "aeb_brand_mapping".
+ *
+ * @property int $id
+ * @property int $brand_id
+ * @property string $name
+ * @property string $created_at
+ */
+class BrandMapping extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'aeb_brand_mapping';
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['brand_id', 'name'], 'required'],
+            [['brand_id'], 'integer'],
+            [['name'], 'string', 'max' => 500],
+        ];
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'brand_id' => Yii::t('app', 'Brand ID'),
+            'name' => Yii::t('app', 'Name'),
+            'created_at' => Yii::t('app', 'Created At'),
+        ];
+    }
+    
+    public function behaviors(): array
+    {
+        return [
+            TimestampBehavior::class => [
+                'class' => TimestampBehavior::class,
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'created_at',
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+}
