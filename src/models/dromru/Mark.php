d35e618272d19cf054308136ce99b60294bea37b
Никита Медведев <n@raidkon.com>
2019-11-19 13:38:59 +0500

    autocrm-8212 В реф базу добавить справочники Avito и Drom.ru + реализ… (#87)
    * autocrm-8212 В реф базу добавить справочники Avito и Drom.ru + реализовать мапер с нашими справоч...
    
    * style
    
    * style
    
    * style
    
    * style

---
+++ src/models/dromru/Mark.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace infotech\reference\models\dromru;
+
+use infotech\reference\models\Brand;
+use infotech\reference\models\dromru\queries\MarkQuery;
+use Yii;
+use infotech\reference\models\ActiveRecord;
+
+/**
+ * This is the model class for table "autocrm.dromru_mark".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $brand_id
+ *
+ * @property Brand $refBrand
+ */
+class Mark extends ActiveRecord
+{
+    public static function tableName(): string
+    {
+        return 'autocrm.dromru_mark';
+    }
+    
+    public function rules(): array
+    {
+        return [
+            [['brand_id'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+    
+    public function attributeLabels(): array
+    {
+        return [
+            'id' => 'ИД (DromRu)',
+            'name' => 'Марка (DromRu)',
+            'brand_id' => 'Бренд',
+        ];
+    }
+    
+    public static function find(): MarkQuery
+    {
+        return new MarkQuery(static::class);
+    }
+    
+    public function getRefBrands()
+    {
+        return $this->hasOne(Brand::class, ['id' => 'brand_id']);
+    }
+}
