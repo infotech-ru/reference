f37eed851eeadfbf2a57ff1342f02b9fce720be9
Vitalik <botchal@gmail.com>
2020-03-17 13:47:27 +0300

    Geely issue 1170 (#103)
    geely issue-1170

---
+++ src/models/carfin/CarfinModel.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace infotech\reference\models\carfin;
+
+use infotech\reference\models\Model;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+
+/**
+ * Модель справочника моделей CarFin
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $brand_id
+ * @property int $model_id
+ *
+ * @property CarfinMark $mark
+ * @property Model $model
+ */
+class CarfinModel extends ActiveRecord
+{
+    /**
+     * @return string
+     */
+    public static function tableName(): string
+    {
+        return 'carfin_model';
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getMark(): ActiveQuery
+    {
+        return $this->hasOne(CarfinMark::class, ['id' => 'brand_id']);
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getModel(): ActiveQuery
+    {
+        return $this->hasOne(Model::class, ['id' => 'model_id']);
+    }
+}
