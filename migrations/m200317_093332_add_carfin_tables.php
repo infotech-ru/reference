db2502e7e1a91a4a7d1e65be9dae748b168e53f1
Botchal <botchal@gmail.com>
2020-03-17 12:57:36 +0300

    geely issue-1170

---
+++ migrations/m200317_093332_add_carfin_tables.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200317_093332_add_carfin_tables
+ */
+class m200317_093332_add_carfin_tables extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('carfin_mark', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(32),
+            'brand_id' => $this->integer(),
+        ]);
+        $this->createTable('carfin_model', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(32),
+            'brand_id' => $this->integer(),
+            'model_id' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('carfin_mark');
+        $this->dropTable('carfin_model');
+    }
+}
