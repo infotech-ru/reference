99802ddddda078eab3989e0a9088fe491798c7fd
Vladislav Kazakov <kazakvvru@gmail.com>
2020-07-03 14:43:34 +0300

    issue #3

---
+++ migrations/m200703_111920_add_modification_id_in_model_alias.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200703_111920_add_modification_id_in_model_alias
+ */
+class m200703_111920_add_modification_id_in_model_alias extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->addColumn('model_alias', 'modification_id', $this->integer());
+
+        $this->addForeignKey(
+            'fk_model_alias_modification_id',
+            'model_alias',
+            'modification_id',
+            'car_modification',
+            'id_car_modification'
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_model_alias_modification_id', 'model_alias');
+        $this->dropColumn('model_alias', 'modification_id');
+    }
+}
--- tests/fixtures/ModelAliasFixture.php
+++ tests/fixtures/ModelAliasFixture.php
@@ -8,6 +8,6 @@ use yii\test\ActiveFixture;
 class ModelAliasFixture extends ActiveFixture
 {
     public $modelClass = ModelAlias::class;
-    public $depends = [BrandFixture::class, ModelFixture::class, GenerationFixture::class, SerieFixture::class];
+    public $depends = [BrandFixture::class, ModelFixture::class, GenerationFixture::class, SerieFixture::class, ModificationFixture::class];
     public $db = 'ref_db';
 }
