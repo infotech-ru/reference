74ee7e4138b290bf9987baac37f11c7acd407cc0
Vitalik <botchal@gmail.com>
2020-08-05 15:09:00 +0300

    Issue wh 3544 (#115)
     issue-wh-3544

---
+++ migrations/m200805_114659_add_tbl_dromru_modification_map.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200805_114659_add_tbl_dromru_modification_map
+ */
+class m200805_114659_add_tbl_dromru_modification_map extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('dromru_model_modification_map', [
+            'dromru_model_id' => $this->integer()->notNull()->comment('ID из dromru_model'),
+            'modification_id' => $this->integer()->notNull()->comment('Наш ID модификации'),
+        ]);
+        $this->addForeignKey(
+            'fk_dromru_model_modification_map_dromru_model_id',
+            'dromru_model_modification_map',
+            'dromru_model_id',
+            'dromru_model',
+            'id'
+        );
+        $this->addForeignKey(
+            'fk_dromru_model_modification_map_generation_id',
+            'dromru_model_modification_map',
+            'modification_id',
+            'car_modification',
+            'id_car_modification'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('dromru_model_modification_map');
+    }
+
+}
