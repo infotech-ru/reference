73cf1a185c8ac8d9cb2f48772085d4f9c2838263
Vladimir Vyachin <vladimir@vyachin.net>
2020-10-01 00:25:55 +0300

    github actions

--- tests/config.php
+++ tests/config.php
@@ -16,7 +16,7 @@ return [
     'components' => [
         'ref_db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=autocrm',
+            'dsn' => 'mysql:host=localhost;dbname=autocrm',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
