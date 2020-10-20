fdc608233ef5825526166bcee54199ab96d3f467
Vladislav Kazakov <kazakvvru@gmail.com>
2020-10-20 14:37:54 +0300

    fix tests

--- tests/unit/models/OptionTest.php
+++ tests/unit/models/OptionTest.php
@@ -56,6 +56,7 @@ class OptionTest extends TestCase
                 'created_at',
                 'updated_at',
                 'option_group_id',
+                'incompatible_options_codes',
             ],
             $model->attributes()
         );
