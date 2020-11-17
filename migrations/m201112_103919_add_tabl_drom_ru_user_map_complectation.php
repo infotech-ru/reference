0729c68c24606c310ab45faaeb17d6adcc42b5a0
Vladimir Vyachin <vladimir@vyachin.net>
2020-11-17 16:19:20 +0300

    ci

--- migrations/m201112_103919_add_tabl_drom_ru_user_map_complectation.php
+++ migrations/m201112_103919_add_tabl_drom_ru_user_map_complectation.php
@@ -7,53 +7,66 @@ use yii\db\Migration;
  */
 class m201112_103919_add_tabl_drom_ru_user_map_complectation extends Migration
 {
-    protected $fk_data = [
-        ['dromru_user_complectation_map', 'user_complectation_id', 'dromru_user_complectation', 'id'],
-        ['dromru_user_complectation_map', 'ref_equipment_id', 'eqt_equipment', 'id'],
-
-        ['dromru_user_modification_map', 'user_complectation_id', 'dromru_user_complectation', 'id'],
-        ['dromru_user_modification_map', 'ref_modification_id', 'car_modification', 'id_car_modification'],
-    ];
-
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->createTable('dromru_user_complectation', [
-            'id' => $this->primaryKey(),
-            'name' => $this->string(),
-            'dromru_model_id' => $this->integer(),
-            'dromru_complectation_id' => $this->integer(),
-        ]);
+        $this->createTable(
+            'dromru_user_complectation',
+            [
+                'id' => $this->primaryKey(),
+                'name' => $this->string(),
+                'dromru_model_id' => $this->integer(),
+                'dromru_complectation_id' => $this->integer(),
+            ]
+        );
 
-        $this->createTable('dromru_user_complectation_map', [
-            'id' => $this->primaryKey(),
-            'user_complectation_id' => $this->integer(),
-            'ref_equipment_id' => $this->integer(),
-        ]);
+        $this->createTable(
+            'dromru_user_complectation_map',
+            [
+                'id' => $this->primaryKey(),
+                'user_complectation_id' => $this->integer(),
+                'ref_equipment_id' => $this->integer(),
+            ]
+        );
 
-        $this->createTable('dromru_user_modification_map', [
-            'id' => $this->primaryKey(),
-            'user_complectation_id' => $this->integer(),
-            'ref_modification_id' => $this->integer(),
-        ]);
-
-        foreach ($this->fk_data as $fk) {
-            $md5name = md5(implode('_', [$fk[0], $fk[1], $fk[2], $fk[3]]));
-            $this->addForeignKey(
-                'FK_' . $md5name,
-                $fk[0],
-                $fk[1],
-                $fk[2],
-                $fk[3]
-            );
-            $this->createIndex(
-                'IDX_' . $md5name,
-                $fk[0],
-                $fk[1]
-            );
-        }
+        $this->createTable(
+            'dromru_user_modification_map',
+            [
+                'id' => $this->primaryKey(),
+                'user_complectation_id' => $this->integer(),
+                'ref_modification_id' => $this->integer(),
+            ]
+        );
+        $this->addForeignKey(
+            'fk_dromru_user_complectation_map_user_complectation_id',
+            'dromru_user_complectation_map',
+            'user_complectation_id',
+            'dromru_user_complectation',
+            'id'
+        );
+        $this->addForeignKey(
+            'fk_dromru_user_complectation_map_ref_equipment_id',
+            'dromru_user_complectation_map',
+            'ref_equipment_id',
+            'eqt_equipment',
+            'id'
+        );
+        $this->addForeignKey(
+            'fk_dromru_user_modification_map_user_complectation_id',
+            'dromru_user_modification_map',
+            'user_complectation_id',
+            'dromru_user_complectation',
+            'id'
+        );
+        $this->addForeignKey(
+            'fk_dromru_user_modification_map_ref_modification_id',
+            'dromru_user_modification_map',
+            'ref_modification_id',
+            'car_modification',
+            'id_car_modification'
+        );
     }
 
     /**
@@ -61,13 +74,15 @@ class m201112_103919_add_tabl_drom_ru_user_map_complectation extends Migration
      */
     public function safeDown()
     {
-        foreach ($this->fk_data as $fk) {
-            $md5name = md5(implode('_', [$fk[0], $fk[1], $fk[2], $fk[3]]));
-            $this->dropForeignKey('FK_' . $md5name, $fk[0]);
-        }
+        $this->dropForeignKey(
+            'fk_dromru_user_complectation_map_user_complectation_id',
+            'dromru_user_complectation_map'
+        );
+        $this->dropForeignKey('fk_dromru_user_complectation_map_ref_equipment_id', 'dromru_user_complectation_map');
+        $this->dropForeignKey('fk_dromru_user_modification_map_user_complectation_id', 'dromru_user_modification_map');
+        $this->dropForeignKey('fk_dromru_user_modification_map_ref_modification_id', 'dromru_user_modification_map');
         $this->dropTable('dromru_user_complectation');
         $this->dropTable('dromru_user_complectation_map');
         $this->dropTable('dromru_user_modification_map');
     }
-
 }
