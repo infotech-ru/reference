18b44101f69665503cf02d3c5e26670320fbb1af
Vladimir Vyachin <vladimir@vyachin.net>
2020-12-29 14:02:25 +0300

    infotech-ru/warehouse#4330 (#128)

---
+++ migrations/m201229_105321_add_column_brand_is_recent.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201229_105321_add_column_brand_is_recent
+ */
+class m201229_105321_add_column_brand_is_recent extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('brands', 'is_recent', $this->boolean()->notNull()->defaultValue(1));
+        $this->alterColumn('brands', 'is_recent', $this->boolean()->notNull());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('brands', 'is_recent');
+    }
+}
