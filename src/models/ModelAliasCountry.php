65da07b9ab1bae9265ee49f4371b497321110529
Vladislav Kazakov <kazakvvru@gmail.com>
2023-01-30 13:28:23 +0300

    Refapp #34 (#178)
    issue infotech-ru/refapp#34

---
+++ src/models/ModelAliasCountry.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace infotech\reference\models;
+
+/**
+ * Class ModelAliasCountry
+ *
+ * @package infotech\reference\models
+ *
+ * @property integer $model_alias_id
+ * @property integer $country_id
+ *
+ * @property ModelAlias $modelAlias
+ * @property Country $country
+ */
+class ModelAliasCountry extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'model_alias_country';
+    }
+
+    public function getModelAlias()
+    {
+        return $this->hasOne(ModelAlias::class, ['id' => 'model_id']);
+    }
+
+    public function getCountry()
+    {
+        return $this->hasOne(Country::class, ['id' => 'country_id']);
+    }
+}
