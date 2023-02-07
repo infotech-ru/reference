ac97e2b1474002e0f78eb5547ced3b6ed7379d77
Vladislav Kazakov <kazakvvru@gmail.com>
2023-02-07 14:35:16 +0300

    issue infotech-ru/dsfsubaru#13801

--- src/models/ModelAliasQuery.php
+++ src/models/ModelAliasQuery.php
@@ -25,4 +25,20 @@ class ModelAliasQuery extends ActiveQuery
     {
         return $this->andWhere([$this->tableName() . '.status' => $status]);
     }
+
+    /**
+     * @param $id
+     * @return ModelAliasQuery
+     * @throws InvalidConfigException
+     */
+    public function country($id)
+    {
+        [$tableName, $alias] = $this->getTableNameAndAlias();
+
+        return $this->andWhere(['EXISTS', ModelAliasCountry::find()->where([
+            'AND',
+            $alias . '.id = ' . ModelAliasCountry::tableName() . '.model_alias_id',
+            [ModelAliasCountry::tableName() . '.country_id' => $id],
+        ])]);
+    }
 }
