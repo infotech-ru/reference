2a261e9745008b15190aae3f2731a5fd709d12b6
artlosk <artlosk@gmail.com>
2023-08-28 15:42:24 +0600

    #issue-16620 Рассылка писем с учетом часовых поясов клиента

--- migrations/m230828_091518_create_table_operator_phones.php
+++ migrations/m230828_091518_create_table_operator_phones.php
@@ -16,6 +16,9 @@ class m230828_091518_create_table_operator_phones extends Migration
             'region_id' => $this->integer()->notNull(),
         ], 'ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci');
 
+        $this->createIndex('idx_from', 'operator_phones', 'from', true);
+        $this->createIndex('idx_to', 'operator_phones', 'to', true);
+
         $this->addForeignKey(
             'fk_operator_phones_region_id_regions',
             'operator_phones',
