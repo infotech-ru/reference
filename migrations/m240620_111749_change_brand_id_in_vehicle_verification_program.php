ad4001063634249747cb682550ce8ff8fb5724e2
Vladislav Kazakov <kazakvvru@gmail.com>
2024-06-20 14:39:14 +0300

    issue infotech-ru/dsfsubaru#22088

--- migrations/m240620_111749_change_brand_id_in_vehicle_verification_program.php
+++ migrations/m240620_111749_change_brand_id_in_vehicle_verification_program.php
@@ -22,5 +22,4 @@ class m240620_111749_change_brand_id_in_vehicle_verification_program extends Mig
     {
         $this->alterColumn('vehicle_verification_program', 'brand_id', $this->integer()->notNull()->unsigned());
     }
-
 }
