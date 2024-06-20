f9c92513563506d8c845282795528a6c0de61858
Vladislav Kazakov <kazakvvru@gmail.com>
2024-06-20 14:40:48 +0300

    issue infotech-ru/dsfsubaru#22088 (#212)
    issue infotech-ru/dsfsubaru#22088

---
+++ migrations/m240620_111749_change_brand_id_in_vehicle_verification_program.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m240620_111749_change_brand_id_in_vehicle_verification_program
+ */
+class m240620_111749_change_brand_id_in_vehicle_verification_program extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('vehicle_verification_program', 'brand_id', $this->integer()->null()->unsigned());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->alterColumn('vehicle_verification_program', 'brand_id', $this->integer()->notNull()->unsigned());
+    }
+}
