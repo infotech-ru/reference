f142ec964d8502ff94c702d224a1ef46a410eb22
BelalWM <belalwm@gmail.com>
2025-12-25 13:16:54 +0300

    issue #43281 (#252)

--- src/models/MtsPhone.php
+++ src/models/MtsPhone.php
@@ -6,11 +6,12 @@ use Yii;
 
 /**
  * @property int $id
- * @property int $hash
+ * @property int|null $hash
  * @property string $phone
- * @property int $request_id
+ * @property int|null $request_id
+ * @property int|null $dealer_id
  * @property int $status
- * @property int $free_at
+ * @property int|null $free_at
  */
 class MtsPhone extends ActiveRecord
 {
